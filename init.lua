eae236574829ee4b8d8f31e4938a0e91c6f4cf6c
technical <technical@fedora.com>
2024-09-11 13:28:58 +0200

    init commit add latex formatting

---
+++ init.lua
@@ -0,0 +1,32 @@
+-- setup lazy.nvim n mason.
+require("config.lazy")
+require("mason").setup()
+
+-- lsp shit
+require("mason-lspconfig").setup()
+require("lspconfig").eslint.setup({})
+
+-- formatting with conform
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		javascript = { "prettierd", "prettier", stop_after_first = true },
+		html = { "prettierd", "prettier", stop_after_first = true },
+		CSS = { "prettierd", "prettier", stop_after_first = true },
+		tex = { "latexindent" },
+	},
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 500,
+		lsp_format = "fallback",
+	},
+})
+
+-- numbering of lines
+vim.wo.number = true
+
+-- Makes tab be 2 spaces. Not sure about the specifics of every single line tbh.
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.expandtab = True
